d72cf01de28de85a44104554738b12eab88ac9d0
Paul-Apo <147522304+Paul-Apo@users.noreply.github.com>
2025-04-24 08:07:22 +0400

    Time: 0 ms (100.00%) | Memory: 18.5 MB (90.27%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,28 +1,15 @@
 class Solution:
-    def twoSum(self,numbers, target: int):
-        for i, num in enumerate(numbers):
-            diff = target - num
-            # Perform binary search for 'diff'
-            l, r = 0, len(numbers) - 1
-            while l <= r:
-                mid = (l + r) // 2
-                if numbers[mid] == diff:
-                    # Ensure we don't use the same index twice
-                    if i != mid:
-                        return [i + 1, mid + 1] if i < mid else [mid + 1, i + 1]
-                    else:
-                        # Check if there's another occurrence of 'diff'
-                        # Search to the left
-                        if mid > 0 and numbers[mid - 1] == diff:
-                            return [i + 1, mid]
-                        # Search to the right
-                        if mid + 1 < len(numbers) and numbers[mid + 1] == diff:
-                            return [i + 1, mid + 2]
-                        break
-                elif diff > numbers[mid]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-        return [-1]
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
 
-            
+        while left < right:
+            total = numbers[left] + numbers[right]
+
+            if total == target:
+                return [left + 1, right + 1]
+
+            elif total > target:
+                right -= 1
+            else:
+                left += 1
