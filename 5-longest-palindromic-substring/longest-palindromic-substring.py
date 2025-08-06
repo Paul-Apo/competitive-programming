8c737f12c23faee0e22314e03c945658e835f8d1
Paul-Apo <147522304+Paul-Apo@users.noreply.github.com>
2025-08-07 02:29:35 +0300

    Time: 311 ms (55.71%) | Memory: 17.8 MB (83.37%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,28 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-    
-        if len(s) < 2:
-            return s
-
-        longest = ""
+        resL = 0
+        resR = 0
+        resLen = 0
         for i in range(len(s)):
-            # Odd length palindrome
-            l1 = self.expandAroundCenter(s, i, i)
-            # Even length palindrome
-            l2 = self.expandAroundCenter(s, i, i + 1)
-            
-            if len(l1) > len(longest):
-                longest = l1
-            if len(l2) > len(longest):
-                longest = l2
+            l, r = i, i 
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    resL = l
+                    resR = r
+                    resLen = r - l + 1
+                l = l - 1
+                r = r + 1
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    resL = l
+                    resR = r
+                    resLen = r - l + 1
+                l = l - 1
+                r = r + 1                
+        return s[resL: resR+ 1]
 
-        return longest
-
-    def expandAroundCenter(self, s, left, right):
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
+    
         
-        # Return the substring from the updated left and right pointers
-        return s[left + 1 : right]
+        
+     
             
