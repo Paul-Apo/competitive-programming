63da7bc4993d6466937e263c3b92cb7bcf9e9bec
Paul-Apo <147522304+Paul-Apo@users.noreply.github.com>
2025-09-10 22:43:06 +0400

    Time: 1525 ms (62.10%) | Memory: 66.8 MB (75.32%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.py
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.py
@@ -2,14 +2,15 @@ class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
         max_xor = 0
         mask = 0
-        for i in range(31, -1, -1):  # check from MSB to LSB
+        for i in range(31, -1, -1):
             mask |= (1 << i)
             prefixes = {num & mask for num in nums}
             candidate = max_xor | (1 << i)
             for p in prefixes:
-                if candidate ^ p in prefixes:
+                if (candidate ^ p) in prefixes:
                     max_xor = candidate
                     break
         return max_xor
+
 
         
