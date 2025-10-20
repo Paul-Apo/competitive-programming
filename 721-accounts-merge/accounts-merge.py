e49edcb4d2142d234ea7c412cbee7beb5666846e
Paul-Apo <147522304+Paul-Apo@users.noreply.github.com>
2025-10-20 23:59:31 +0400

    Time: 31 ms (52.97%) | Memory: 23.2 MB (27.60%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -36,5 +36,5 @@ class Solution:
             if email not in visited:
                 merged_emails = dfs(email)
                 res.append([email_to_name[email]] + sorted(merged_emails))
-        res10 = res
-        return res10
+        res11 = res
+        return res11
