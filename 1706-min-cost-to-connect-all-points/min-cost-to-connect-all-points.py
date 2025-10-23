ee7e2b265b3b3041cca924d405d5859afe081a1f
Paul-Apo <147522304+Paul-Apo@users.noreply.github.com>
2025-10-24 00:09:59 +0400

    Time: 2451 ms (30.55%) | Memory: 178.5 MB (13.23%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -2,27 +2,24 @@ class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
         adj = {i: [] for i in range(n)}
+
         for i in range(n):
             x1, y1 = points[i]
             for j in range(i+1, n):
                 x2, y2 = points[j]
-                dist = abs(x2-x1) + abs(y2 - y1)
-                adj[i].append ([dist, j])
+                dist = abs(x1 -x2) + abs(y1 - y2)
+                adj[i].append([dist, j])
                 adj[j].append([dist, i])
         res = 0
-        minHeap = [[0,0]]
+        minH = [[0, 0]]
         visit = set()
-
         while len(visit) < n:
-            cost, i = heapq.heappop(minHeap)
+            cost, i = heapq.heappop(minH)
             if i in visit:
                 continue
             res += cost
             visit.add(i)
             for neiCost, nei in adj[i]:
                 if not nei in visit:
-                    heapq.heappush(minHeap, [neiCost, nei])
+                    heapq.heappush(minH, [neiCost, nei])
         return res
-
-
-        
