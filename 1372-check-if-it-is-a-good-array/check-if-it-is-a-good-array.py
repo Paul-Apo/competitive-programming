acf39613faba3fe0569d9b832f27502f2fcb840f
Paul-Apo <147522304+Paul-Apo@users.noreply.github.com>
2025-11-14 23:45:55 +0400

    Time: 4 ms (68.99%) | Memory: 29 MB (64.08%) - LeetSync

--- 1372-check-if-it-is-a-good-array/check-if-it-is-a-good-array.py
+++ 1372-check-if-it-is-a-good-array/check-if-it-is-a-good-array.py
@@ -3,5 +3,5 @@ class Solution:
         g = nums[0]
         for v in nums[1:]:
             g = gcd(g, v)
-        res4 = (g == 1)
-        return res4
+        res5 = (g == 1)
+        return res5
